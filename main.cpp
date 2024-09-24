2bd620e47684b1ed8a4bc64777dedfaeb9c9c8d7
myroom123123123 <mroom9195@gmail.com>
2024-09-24 20:21:20 +0300

    ex2

--- main.cpp
+++ main.cpp
@@ -6,29 +6,46 @@ private:
 	double real;
 	double imag;
 public:
-	Complex() : real(0), imag(0) {}
-	Complex(double r, double i) : real(r), imag(i) {}
+	Complex& operator=(const Complex& c) {
+		real = c.real;
+		imag = c.imag;
+		return *this;
+	}
 	Complex operator+(const Complex& c) {
-		return Complex(real + c.real, imag + c.imag);
+		Complex temp;
+		temp.real = real + c.real;
+		temp.imag = imag + c.imag;
+		return temp;
 	}
 	Complex operator-(const Complex& c) {
-		return Complex(real - c.real, imag - c.imag);
+		Complex temp;
+		temp.real = real - c.real;
+		temp.imag = imag - c.imag;
+		return temp;
 	}
 	Complex operator*(const Complex& c) {
-		return Complex(real * c.real - imag * c.imag, real * c.imag + imag * c.real);
+		Complex temp;
+		temp.real = real * c.real - imag * c.imag;
+		temp.imag = real * c.imag + imag * c.real;
+		return temp;
 	}
 	Complex operator/(const Complex& c) {
-		return Complex((real * c.real + imag * c.imag) / (c.real * c.real + c.imag * c.imag), (imag * c.real - real * c.imag) / (c.real * c.real + c.imag * c.imag));
+		Complex temp;
+		temp.real = (real * c.real + imag * c.imag) / (c.real * c.real + c.imag * c.imag);
+		temp.imag = (imag * c.real - real * c.imag) / (c.real * c.real + c.imag * c.imag);
+		return temp;
 	}
+	Complex(double r = 0, double i = 0) : real(r), imag(i) {}
 	void print() {
-		cout << real << " + " << imag << "i" << endl;
+		ostream& out = cout;
+		out << real << " + " << imag << "i" << endl;
+
 	}
 };
 
 int main(){
-	Complex c1(1, 2);
-	Complex c2(3, 4);
-	Complex c3 = c1 + c2;
+	Complex c1(1, 2), c2(3, 4), c3;
+	c3 = c1 + c2;
 	c3.print();
 	c3 = c1 - c2;
 	c3.print();
